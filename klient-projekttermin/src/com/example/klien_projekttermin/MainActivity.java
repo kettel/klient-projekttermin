3963243868a0afbe17adc1b494b6867e2dcdc61b
Kettel <wiktor@kettel.se>
2012-10-30 09:45:18 +0100

    Tutorial för sqlite fungerar

--- klient-projekttermin/src/com/example/klien_projekttermin/MainActivity.java
+++ klient-projekttermin/src/com/example/klien_projekttermin/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.klien_projekttermin;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -10,11 +13,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Startar: ", "Main");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void testDB(View view){
+    	Intent intent = new Intent(this, DBTest.class);
+    	Log.d("Startar: ", "TestDB");
+    	startActivity(intent);
     }
 }
