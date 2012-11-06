5b0f8bd277aefc4fb40d1f94a74e81fc76ea52f1
kettel <wiktor@kettel.se>
2012-11-06 14:03:35 +0100

    Rensat lite DB-test rester

--- klient-projekttermin/src/com/example/klien_projekttermin/MainActivity.java
+++ klient-projekttermin/src/com/example/klien_projekttermin/MainActivity.java
@@ -1,26 +1,16 @@
 package com.example.klien_projekttermin;
 
 import java.util.ArrayList;
-import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
-
 import logger.LogViewer;
 import logger.logger;
-import models.Assignment;
-import models.Contact;
-import models.MessageModel;
-import models.ModelInterface;
 
-import database.Database;
 
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.format.Time;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
