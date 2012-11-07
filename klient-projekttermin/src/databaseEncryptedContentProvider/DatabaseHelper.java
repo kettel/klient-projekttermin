c5452135c125d272b0fb253bfa19f093e0594389
kettel <wiktor@kettel.se>
2012-11-07 16:31:08 +0100

    Jag bör har skapat ett exempel. Kommit till punkt 8.5 Resources på vogella.com.

---
+++ klient-projekttermin/src/databaseEncryptedContentProvider/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package databaseEncryptedContentProvider;
+
+import android.content.Context;
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+	private static final String DATABASE_NAME = "tddd36.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
