99495d39e7d3f654b412af3c78416a7c51b9f949
Wiktor Kettel <wiktor@kettel.se>
2012-11-12 08:20:41 +0100

    Försöker skapa en snabbare provider...

---
+++ klient-projekttermin/src/com/example/klien_projekttermin/databaseProviderFaster/AssignmentTable.java
@@ -0,0 +1,51 @@
+package com.example.klien_projekttermin.databaseProviderFaster;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import android.util.Log;
+
+public class AssignmentTable {
+	// Assignments tabellnamn
+	public static final String TABLE_ASSIGNMENTS = "assignment";
+
+	// Assignments tabellkolumnnamn
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_LAT = "lat";
+	public static final String COLUMN_LON = "long";
+	public static final String COLUMN_RECEIVER = "receiver";
+	public static final String COLUMN_SENDER = "sender";
+	public static final String COLUMN_ASSIGNMENTDESCRIPTION = "description";
+	public static final String COLUMN_TIMESPAN = "timespan";
+	public static final String COLUMN_ASSIGNMENTSTATUS = "assignment_status";
+	public static final String COLUMN_CAMERAIMAGE = "camera_image";
+	public static final String COLUMN_STREETNAME ="streetname";
+	public static final String COLUMN_SITENAME = "sitename";
+
+	// Database creation SQL statement
+	private static final String DATABASE_CREATE = "CREATE TABLE IF NOT EXISTS " 
+			+ TABLE_ASSIGNMENTS + "("
+            + Database.KEY_ID + " integer primary key autoincrement,"  
+    		+ COLUMN_NAME + " TEXT,"
+            + COLUMN_LAT + " TEXT,"
+    		+ COLUMN_LON + " TEXT,"
+            + COLUMN_RECEIVER + " TEXT,"
+            + COLUMN_SENDER + " TEXT,"
+            + COLUMN_ASSIGNMENTDESCRIPTION + " TEXT,"
+            + COLUMN_TIMESPAN + " TEXT,"
+            + COLUMN_ASSIGNMENTSTATUS + " TEXT,"
+            + COLUMN_CAMERAIMAGE + " BLOB,"
+            + COLUMN_STREETNAME + " TEXT,"
+            + COLUMN_SITENAME + " TEXT" + ")";
+
+	public static void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+		Log.w(MessageTable.class.getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS " + TABLE_ASSIGNMENTS);
+		onCreate(database);
+	}
+}
---
+++ klient-projekttermin/src/com/example/klien_projekttermin/databaseProviderFaster/ContactsTable.java
@@ -0,0 +1,42 @@
+package com.example.klien_projekttermin.databaseProviderFaster;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import android.util.Log;
+
+public class ContactsTable {
+
+
+	// Database table
+	public static final String TABLE_CONTACTS = "contact";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_CONTACT_NAME = "contact_name";
+	public static final String COLUMN_PH_NO = "phone_number";
+	public static final String COLUMN_EMAIL = "email";
+	public static final String COLUMN_CLEARANCE_LEVEL = "clearance_level";
+	public static final String COLUMN_CLASSIFICATION = "classification";
+	public static final String COLUMN_COMMENT = "comment";
+
+	// Database creation SQL statement
+	private static final String DATABASE_CREATE = "CREATE TABLE IF NOT EXISTS " 
+			+ TABLE_CONTACTS + "("
+            + Database.KEY_ID + " integer primary key autoincrement, "  
+    		+ COLUMN_CONTACT_NAME + " TEXT, "
+            + COLUMN_PH_NO + " TEXT, "
+    		+ COLUMN_EMAIL + " TEXT, "
+            + COLUMN_CLEARANCE_LEVEL + " TEXT, "
+            + COLUMN_CLASSIFICATION + " TEXT, "
+            + COLUMN_COMMENT + " TEXT)";
+
+	public static void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	public static void onUpgrade(SQLiteDatabase database, int oldVersion,
+			int newVersion) {
+		Log.w(MessageTable.class.getName(), "Upgrading database from version "
+				+ oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+		onCreate(database);
+	}
+}
