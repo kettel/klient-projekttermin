e350331e54382fe04af42f1cb012854afcf9bb7a
kettel <wiktor@kettel.se>
2012-11-14 08:45:07 +0100

    Kommenterat enum

--- klient-projekttermin/src/models/AssignmentStatus.java
+++ klient-projekttermin/src/models/AssignmentStatus.java
@@ -1,9 +1,13 @@
 package models;
+
 /**
  * Enum för uppdragsstatus.
+ * 
  * @author kettel
- *
+ * 
  */
 public enum AssignmentStatus {
-	NOT_STARTED,STARTED,NEED_HELP,FINISHED
+	// Ett uppdrag är antingen icke påbörjat (NOT_STARTED), påbörjat (STARTED),
+	// behöver hjälp (NEED_HELP) samt avslutat (FINISHED)
+	NOT_STARTED, STARTED, NEED_HELP, FINISHED
 }
