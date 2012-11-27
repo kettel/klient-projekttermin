fe529f6ffc09a35209a4695f708c0009bc2707bc
Eric Lundmark <erilu506@gmail.com>
2012-11-27 17:49:01 +0100

    skapade SocketConnection

---
+++ klient-projekttermin/src/communicationModule/SocketConnection.java
@@ -0,0 +1,18 @@
+package communicationModule;
+
+import java.util.Observable;
+
+import models.AuthenticationModel;
+import models.ModelInterface;
+
+public class SocketConnection extends Observable{
+
+	public void sendModel(ModelInterface m) {
+		
+	}
+
+	public void authenticate(AuthenticationModel authenticationModel) {
+		
+	}
+
+}
